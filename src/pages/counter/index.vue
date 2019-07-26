3b6f985c58268169baed11f43e8d33049bead6f4
董华锐 <ezreal.ma@foxmail.com>
2019-07-26 17:27:47 +0800

    mpvue的小程序

---
+++ src/pages/counter/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="counter-warp">
+    <p>Vuex counter：{{ count }}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+      <button @click="getPosition">获取当前位置</button>
+    </p>
+  </div>
+</template>
+
+<script>
+// Use Vuex
+import store from './store'
+
+export default {
+  computed: {
+    count () {
+      return store.state.count
+    }
+  },
+  methods: {
+    increment () {
+      store.commit('increment')
+    },
+    decrement () {
+      store.commit('decrement')
+    },
+    getPosition () {
+      mpvue.getLocation({
+        type: 'wgs84',
+        success: (res) => {
+          var latitude = res.latitude // 纬度
+          var longitude = res.longitude // 经度
+          console.log('latitude', latitude)
+          console.log('longitude', longitude)
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.counter-warp {
+  text-align: center;
+  margin-top: 100px;
+}
+.home {
+  display: inline-block;
+  margin: 100px auto;
+  padding: 5px 10px;
+  color: blue;
+  border: 1px solid blue;
+}
+</style>
